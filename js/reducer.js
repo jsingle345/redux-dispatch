215640ff6747457f27b9967ee39ec0c0ab5df9b1
jsingle345 <jsingle345@gmail.com>
2020-11-30 14:12:03 -0600

    commit tree 2ebd106b00e2aeced2d338d8bcaaa514f93a1805

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,6 @@
+
+let state = {count: 0}
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +10,26 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+// let action = {type: 'INCREASE_COUNT'}
+
+// state = changeState(state, action)
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+
+function render(){
+  document.body.textContent = state.count 
+}
+
+let button = document.createElement('BUTTON')
+  // button.addEventListener('click', () => {
+  //   console.log('click')
+  // })
+  button.innerText = "INCREASE"
+
+document.body.append(button)
+render()
